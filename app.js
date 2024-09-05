e1f5e008747014e9a23c32e12851302a56453458
Anushka Sharma <anushka626sata@gmail.com>
2024-09-05 02:55:47 -0400

    created components on homepage

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import logo from "./assets/logo-eato.jpg";
-import image from "./assets/user-icon.jpg";
+import userIcon from "./assets/user-icon.jpg";
+import cartIcon from "./assets/shopping-cart.jpg";
+import resImage from "./assets/pizza-pizza.jpg";
 
 const HeaderComponent = () => {
     return (
@@ -14,11 +16,46 @@ const HeaderComponent = () => {
                     <li>Home</li>
                     <li>About Us</li>
                     <li>Contact Us</li>
-                    <li>Search</li>
                 </ul>
             </div>
             <div className="cartContainer">
-                <img src={image} alt="user-icon" className="userIcon" />
+                <img src={userIcon} alt="user-icon" className="userIcon" />
+                <img src={cartIcon} alt="cart-icon" className="cartIcon" />
+            </div>
+        </div>
+    );
+};
+
+const RestaurantCard = (props) => {
+    return (
+        <div className="res-card">
+            <div className="res-image-box">
+                <img src={resImage} alt="restaurant-image" className="res-img" />
+            </div>
+            <div className="res-details">
+                <h3 style={{margin: '0px'}}>{props.resName}</h3>
+                <p style={{margin: '0px'}}>Pizzas, Italian, Pastas, Desserts</p>
+            </div>
+        </div>
+    );
+};
+
+const BodyComponent = () => {
+    return (
+        <div className="bodyContainer">
+            <div className="searchContainer">
+                <input 
+                    type="text" 
+                    className="search-input"
+                    placeholder="Search..." 
+                />
+                <button className="search-btn">Search</button>
+            </div>
+            <div className="restaurantContainer">
+                <RestaurantCard resName="Pizza Pizza"/>
+                <RestaurantCard resName="KFC"/>
+                <RestaurantCard resName="Poulet Rouge"/>
+                <RestaurantCard resName="Masakali"/>
             </div>
         </div>
     );
@@ -28,6 +65,7 @@ const AppLayout = () => {
     return (
         <div className="app">
             <HeaderComponent />
+            <BodyComponent />
         </div>
     );
 };
