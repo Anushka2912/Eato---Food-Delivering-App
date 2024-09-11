0b98e869b3e1b1c20f37765884e6882a0b4fda69
Anushka Sharma <anushka626sata@gmail.com>
2024-09-11 14:03:16 -0400

    misc: various small updates across the project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,32 @@
-import logo from '../images/logo-eato.jpg';
-import userIcon from '../images/user-icon.jpg';
-import cartIcon from '../images/shopping-cart.jpg';
+import logo from "../images/logo-eato.jpg";
+import userIcon from "../images/user-icon.jpg";
+import cartIcon from "../images/shopping-cart.jpg";
 
 const HeaderComponent = () => {
-    return (
-        <div className="headerContainer">
-            <div className="logoContainer">
-                <img src={logo} alt="company-logo-image" className="mainLogo" />
-            </div>
-            <div className="navContainer">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About Us</a></li>
-                    <li><a href="">Contact Us</a></li>
-                </ul>
-            </div>
-            <div className="cartContainer">
-                <img src={userIcon} alt="user-icon" className="userIcon" />
-                <img src={cartIcon} alt="cart-icon" className="cartIcon" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="headerContainer">
+      <div className="logoContainer">
+        <img src={logo} alt="company-logo-image" className="mainLogo" />
+      </div>
+      <div className="navContainer">
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div className="cartContainer">
+        <img src={userIcon} alt="user-icon" className="userIcon" />
+        <img src={cartIcon} alt="cart-icon" className="cartIcon" />
+      </div>
+    </div>
+  );
 };
 
 export default HeaderComponent;
