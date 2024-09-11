4730ca471f78968ef65e148e6906a281ae6f54b2
Anushka Sharma <anushka626sata@gmail.com>
2024-09-11 13:24:30 -0400

    feat(filter): add filter buttons for 'All Restaurants' and 'Top Rated Restaurants'

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import searchIcon from '../images/search-icon.jpg';
 import resList from '../utils/mockData';
 import RestaurantCard from './ResataurantCard';
 
 const BodyComponent = () => {
+
+    const [topRestaurants, setTopRestaurants] = useState(resList);
+
     return (
         <div className="bodyContainer">
             <div className="searchContainer">
@@ -13,8 +17,29 @@ const BodyComponent = () => {
                     placeholder="Search..." 
                 />
             </div>
+            <div className="filtersContainer">
+                <button 
+                    className="all-filter-btn global-btn" 
+                    onClick={() => {
+                        setTopRestaurants(resList);
+                    }}
+                >
+                All
+                </button>  
+                <button 
+                    className="top-filter-btn global-btn" 
+                    onClick={() => {
+                        const filteredList = resList.filter(
+                            (restaurant) => restaurant.data.rating>4.0
+                        );
+                        setTopRestaurants(filteredList);
+                    }}
+                >
+                Top Rated Restaurants
+                </button>                
+            </div>
             <div className="restaurantContainer">
-                {resList.map((restaurant) => (
+                {topRestaurants.map((restaurant) => (
                     <RestaurantCard key={restaurant.data.id} resData={restaurant} />
                 ))}
             </div>
