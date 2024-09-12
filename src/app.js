3bd425088cfa9e5ab165fa2567daaaeded19e473
Anushka Sharma <anushka626sata@gmail.com>
2024-09-11 22:41:55 -0400

    feat(footer): create footer component with basic structure

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import HeaderComponent from "./components/Header";
 import BodyComponent from "./components/Body";
+import FooterComponent from "./components/Footer";
 
 const AppLayout = () => {
     return (
         <div className="app">
             <HeaderComponent />
             <BodyComponent />
+            <FooterComponent />
         </div>
     );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,75 @@
+const FooterComponent = () => {
+    return (
+        <div className="footerContainer">
+            {/* <div className="footer-top">
+                <div className="newsletter-section">
+
+                </div>
+            </div> */}
+            <div className="footer-middle">
+                <div className="company-information-section">
+                    <div className="company-logo">
+                        <img></img>
+                    </div>
+                    <div className="social-links">
+                        <img></img>
+                        <img></img>
+                        <img></img>
+                        <img></img>
+                    </div>
+                </div>
+                <div className="quick-links-section">
+                    <div className="footer-title">
+                        <h5>Quick Links</h5>
+                    </div>
+                    <div className="footer-navigation">
+
+                    </div>
+                </div>
+                <div className="useful-links-section">
+                    <div className="footer-title">
+                        <h5>Useful Links</h5>
+                    </div>
+                    <div className="footer-navigation">
+                        
+                    </div>
+                </div>
+                <div className="contact-section">
+                    <div className="footer-title">
+                        <h5>Contact Us</h5>
+                    </div>
+                    <div className="contact-navigation">
+                        
+                    </div>
+                </div>
+                <div className="download-app-section">
+                    <div className="footer-title">
+                        <h5>Download App</h5>
+                    </div>
+                    <div className="download-app-icons">
+
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <div className="copyright-section">
+                    <p>© 2024, Eato, All Rights Reserved</p>
+                </div>
+                <div className="payment-section">
+                    <p>Accept for</p>
+                    <div className="paypal-icon">
+                        <img></img>
+                    </div>
+                    <div className="visa-icon">
+                        <img></img>
+                    </div>
+                    <div className="mastercard-icon">
+                        <img></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FooterComponent;
