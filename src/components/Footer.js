d02314037137b1adc2c2a9058f2d8a599b822f11
Anushka Sharma <anushka626sata@gmail.com>
2024-09-12 13:10:33 -0400

    feat: add responsive styling for footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import logo from "../images/logo-eato.jpg";
+import footerLogo from "../images/logo-eato-footer.png";
 
 const FooterComponent = () => {
     return (
@@ -9,79 +9,83 @@ const FooterComponent = () => {
                 </div>
             </div> */}
             <div className="footer-middle">
-                <div className="company-information-section footer-div">
-                    <div className="company-logo">
-                        <img src={logo} alt="company-logo-image" className="mainLogo" />
+                <div className="footer-inner-row">
+                    <div className="footer-div" id="company-information-section">
+                        <div className="company-logo">
+                            <img src={footerLogo} alt="company-logo-image" className="mainLogo" />
+                        </div>
+                        <p>Providing the best food experience, straight to your door.</p>
+                        <div className="social-links">
+                            <img></img>
+                            <img></img>
+                            <img></img>
+                            <img></img>
+                        </div>
                     </div>
-                    <p>Providing the best food experience, straight to your door.</p>
-                    <div className="social-links">
-                        <img></img>
-                        <img></img>
-                        <img></img>
-                        <img></img>
+                    <div className="footer-div" id="quick-links-section">
+                        <div className="footer-title">
+                            <h4>Quick Links</h4>
+                        </div>
+                        <div className="footer-navigation">
+                            <ul>
+                                <li><a href="#">Link 1</a></li>
+                                <li><a href="#">Link 2</a></li>
+                                <li><a href="#">Link 3</a></li>
+                                <li><a href="#">Link 4</a></li>
+                                <li><a href="#">Link 5</a></li>
+                                <li><a href="#">Link 6</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div className="quick-links-section footer-div">
-                    <div className="footer-title">
-                        <h4>Quick Links</h4>
+                    <div className="footer-div" id="useful-links-section">
+                        <div className="footer-title">
+                            <h4>Useful Links</h4>
+                        </div>
+                        <div className="footer-navigation">
+                            <ul>
+                                <li><a href="#">Link 1</a></li>
+                                <li><a href="#">Link 2</a></li>
+                                <li><a href="#">Link 3</a></li>
+                                <li><a href="#">Link 4</a></li>
+                                <li><a href="#">Link 5</a></li>
+                                <li><a href="#">Link 6</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="footer-navigation">
-                        <ul>
-                            <li><a href="#">Link 1</a></li>
-                            <li><a href="#">Link 2</a></li>
-                            <li><a href="#">Link 3</a></li>
-                            <li><a href="#">Link 4</a></li>
-                            <li><a href="#">Link 5</a></li>
-                            <li><a href="#">Link 6</a></li>
-                        </ul>
+                    <div className="footer-div" id="contact-section">
+                        <div className="footer-title">
+                            <h4>Contact Us</h4>
+                        </div>
+                        <div className="contact-navigation">
+                            
+                        </div>
                     </div>
-                </div>
-                <div className="useful-links-section footer-div">
-                    <div className="footer-title">
-                        <h4>Useful Links</h4>
-                    </div>
-                    <div className="footer-navigation">
-                        <ul>
-                            <li><a href="#">Link 1</a></li>
-                            <li><a href="#">Link 2</a></li>
-                            <li><a href="#">Link 3</a></li>
-                            <li><a href="#">Link 4</a></li>
-                            <li><a href="#">Link 5</a></li>
-                            <li><a href="#">Link 6</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="contact-section footer-div">
-                    <div className="footer-title">
-                        <h4>Contact Us</h4>
-                    </div>
-                    <div className="contact-navigation">
-                        
-                    </div>
-                </div>
-                <div className="download-app-section footer-div">
-                    <div className="footer-title">
-                        <h4>Download App</h4>
-                    </div>
-                    <div className="download-app-icons">
+                    <div className="footer-div" id="download-app-section">
+                        <div className="footer-title">
+                            <h4>Download App</h4>
+                        </div>
+                        <div className="download-app-icons">
 
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="footer-bottom">
-                <div className="copyright-section">
-                    <p>© 2024, Eato, All Rights Reserved</p>
-                </div>
-                <div className="payment-section">
-                    <p>Accept for</p>
-                    <div className="paypal-icon">
-                        <img></img>
+                <div className="footer-inner-row">
+                    <div className="copyright-section">
+                        <p>© 2024, Eato, All Rights Reserved</p>
                     </div>
-                    <div className="visa-icon">
-                        <img></img>
-                    </div>
-                    <div className="mastercard-icon">
-                        <img></img>
+                    <div className="payment-section">
+                        <p>Accept for</p>
+                        <div className="paypal-icon">
+                            <img></img>
+                        </div>
+                        <div className="visa-icon">
+                            <img></img>
+                        </div>
+                        <div className="mastercard-icon">
+                            <img></img>
+                        </div>
                     </div>
                 </div>
             </div>
