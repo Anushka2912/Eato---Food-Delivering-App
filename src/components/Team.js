712abba22cdaadb32244f028f126aa12fb9d1a95
Anushka Sharma <anushka626sata@gmail.com>
2024-10-13 16:02:34 -0400

    feat: add team component to display team members using class components Implemented a team component to showcase team members on the About page, utilizing class-based components for structure.

---
+++ src/components/Team.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class TeamClass extends React.Component {
+    render ()
+    {
+        return (
+            <div className="team-card">
+                <h2>Name: Anushka Sharma</h2>
+                <h3>Location: Ottawa, Canada</h3>
+                <h4>Contact: anushka.sharma.cn@gmail.com</h4>
+            </div>
+        );
+    }
+}
+
+export default TeamClass;
