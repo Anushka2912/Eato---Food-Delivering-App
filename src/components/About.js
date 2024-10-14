7a8571f8c1322e57ab02dd5951b27f88d467f520
Anushka Sharma <anushka626sata@gmail.com>
2024-10-14 01:02:47 -0400

    feat(about): added Team component in the about us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
+import TeamClass from "./Team";
 const AboutComponent = () => {
+
     return (
         <div className="mainContainer">
             <div className="aboutContainer">
                 <h2>About Page</h2>
+                <TeamClass />
             </div>
         </div>
     );
