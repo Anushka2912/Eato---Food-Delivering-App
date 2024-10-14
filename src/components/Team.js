1e964fb22ffba1292b3b32bec673f5aaeab0e45b
Anushka Sharma <anushka626sata@gmail.com>
2024-10-14 01:04:03 -0400

    feat(team): added data using api call to github

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,13 +1,38 @@
 import React from "react";
 
 class TeamClass extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userInfo: {
+                name: "userName",
+                location: "userLoc",
+                email: "userEmail",
+            },
+        };
+    }
+
+    async componentDidMount() {
+        const data = await fetch("https://api.github.com/users/aanushkasharma");
+        const json = await data.json();
+
+        console.log(json);
+        this.setState( {
+            userInfo: json,
+        });
+    }
+
     render ()
     {
+        const { name, location, email } = this.state.userInfo;
+        debugger;
         return (
             <div className="team-card">
-                <h2>Name: Anushka Sharma</h2>
-                <h3>Location: Ottawa, Canada</h3>
-                <h4>Contact: anushka.sharma.cn@gmail.com</h4>
+                <h3>Name: {name || "No name available"}</h3>
+                <h3>Location: {location || "No location available"}</h3>
+                <h3>Email: {email || "No email available"}</h3>
             </div>
         );
     }
