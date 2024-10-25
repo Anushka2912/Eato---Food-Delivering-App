31da3d39a5aa01dc16dde70baa7553a57a0450ff
Anushka Sharma <anushka626sata@gmail.com>
2024-10-24 21:59:20 -0400

    refactor: add tailwind css in shimmer component

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,6 +1,6 @@
 const Shimmer = () => {
     return (
-        <div className="shimmer-container">
+        <div className="flex flex-wrap justify-between gap-y-9 gap-x-9 shimmer-container">
             <div className="w-[280px] h-[300px] border border-[#d0d0d0] rounded-2xl shimmer-card">
                 <div className="bg-[#e8e8e8] w-full h-[215px] rounded-t-2xl shimmer-image-box"></div>
             </div>
