57c78043ede0cc2602dfa03dcada02bb62bc9027
Anushka Sharma <anushka626sata@gmail.com>
2024-10-29 15:24:18 -0400

    minor code changes

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,12 +1,13 @@
+import ItemList from "./ItemList";
 
 const RestaurantCategory = ({data}) => {
     return (
         <div className="w-9/12 m-auto categoryContainer">
             <div className="bg-slate-200 shadow-lg p-4 my-4 accordionHeader">
-                <p className="font-bold">{data.title} ({data.itemCards.length})</p>
+                <p className="text-xl font-bold">{data.title} ({data.itemCards.length})</p>
             </div>
-            <div className="accordionBody">
-                <p>body</p>
+            <div className="p-4 accordionBody">
+                <ItemList items={data.itemCards} />
             </div>
         </div>
     )
