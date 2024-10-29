d46b01cb65a51de3b364910bc3b45a2368ae069c
Anushka Sharma <anushka626sata@gmail.com>
2024-10-29 14:21:34 -0400

    refactor: optimize code

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
 
@@ -9,20 +10,37 @@ const RestaurantMenu = () => {
 
     const resMenuInfo = useRestaurantMenu(resId);
 
-
     console.log({resMenuInfo});
+    
+    const text = resMenuInfo?.data?.cards?.[0]?.card?.card?.text;
+
+    const title = resMenuInfo?.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card?.card?.title;
+
+    const itemCards = resMenuInfo?.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card?.card?.itemCards;
+
+    const categories = resMenuInfo?.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+        (c) => c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+
+    console.log( {categories} );
+
     return resMenuInfo === null ? (
         <Shimmer />
     ) : (
         <div className="mainContainer">
             <div className="RestaurantHeader flex justify-center items-center bg-slate-300 min-h-96">
-                <h2 className="text-4xl font-bold p-5">{resMenuInfo.data.cards[0].card.card.text}</h2>
+                <h2 className="text-4xl font-bold p-5">{text}</h2>
             </div> 
+            <div className="accordianContainer">
+                {categories.map((cat, index) => (
+                    <RestaurantCategory key={index} data={cat.card?.card}/>
+                ))}
+            </div>
             <div className="RestaurantMenu w-full flex px-9 py-20 m-auto">
                 <div className="RestaurantMenuContainer w-fit max-w-[1500px] m-auto border border-[#00000026]">
-                    <h3 className="bg-red-600 text-white text-2xl font-semibold p-5 mb-5">{resMenuInfo.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.title}</h3>
+                    <h3 className="bg-red-600 text-white text-2xl font-semibold p-5 mb-5">{title}</h3>
                     <ul className="flex flex-col justify-center gap-y-11 gap-x-11 p-5">
-                        {resMenuInfo.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards.map((item) => (
+                        {itemCards.map((item) => (
                             <li key={item.card.info.id} className="flex place gap-10 align-top menuListItem">
                                 <div className="flex flex-col gap-y-4 w-3/5">
                                     <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/" + item.card.info.imageId} className="w-full max-w-[200px]"></img>
