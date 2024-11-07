dd8f54a034d2773696d2921ac766898eb0c4b6a9
Anushka Sharma <anushka626sata@gmail.com>
2024-11-07 01:25:42 -0500

    minor code changes

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -5,7 +5,7 @@ import "@testing-library/jest-dom";
 test("Should load Contact Component", () => {
     render(<Contact />);
 
-    const heading = screen.getByRole("heading");
+    const heading = screen.getAllByRole("heading");
 
     expect(heading).toBeInTheDocument();
 });
