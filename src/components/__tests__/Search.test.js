ce957c2f636beb4685f0ba95273a7b01afffd261
Anushka Sharma <anushka626sata@gmail.com>
2024-11-09 19:20:05 -0500

    test: add test cases for Search component

---
+++ src/components/__tests__/Search.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import BodyComponent from "../Body";
+
+test("Should render the body component with the Search bar", () => {
+    render(<BodyComponent />);
+});
